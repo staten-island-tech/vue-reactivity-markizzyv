a9ee83cff1511b1e0613dd35c7446f8166194bff
markizzyv <144455870+markizzyv@users.noreply.github.com>
2024-03-06 09:12:03 -0500

    fjfu
    f

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -7,15 +7,10 @@
       @addToCart="AddItem(vinyl)"
 />
  </div>
-<<<<<<< HEAD
-
-
-=======
  <div>
   <VinylCart v-show="cart > 0"
     />
  </div>
->>>>>>> parent of 9498383 (s)
 </template>
 
 <script setup>
